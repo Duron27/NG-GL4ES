04d3acdd2f7b06085d23ae225ae977b2b7429b98
Sisah2 <64099446+Sisah2@users.noreply.github.com>
2025-06-09 01:15:52 +0200

    Update shaderconv.c

--- src/gl/vgpu/shaderconv.c
+++ src/gl/vgpu/shaderconv.c
@@ -442,7 +442,7 @@ vec2 vgpu_step(vec2 x, vec2 y) { return step(x, y); }\n\
 vec3 vgpu_step(float x, vec3 y) { return step(x, y); }\n\
 vec3 vgpu_step(int x, vec3 y) { return step(float(x), y); }\n\
 vec3 vgpu_step(vec3 x, vec3 y) { return step(x, y); }\n\
-vec4 vgpu_step(float x, vec2 y) { return step(x, y); }\n\
+vec4 vgpu_step(float x, vec4 y) { return step(x, y); }\n\
 vec4 vgpu_step(int x, vec4 y) { return step(float(x), y); }\n\
 vec4 vgpu_step(vec4 x, vec4 y) { return step(x, y); }\n\
 float vgpu_exp2(float x) { return exp2(x); }\n\
